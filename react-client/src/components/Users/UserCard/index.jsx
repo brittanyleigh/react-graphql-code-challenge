f3d059026c10b73256c03ad184b212224a5ba9af
Brittany Isenberg <brittany.isenberg@gmail.com>
2020-11-27 13:31:44 -0500

    finish styling

--- react-client/src/components/Users/UserCard/index.jsx
+++ react-client/src/components/Users/UserCard/index.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import Card from "../../core/Card";
 import styles from "./userCard.module.css";
+import { FaEnvelope, FaPhone } from "react-icons/fa";
 
-const colors = ["red", "green", "yellow", "blue"];
+const colors = ["red", "yellow", "green", "blue"];
 
-const LoadingCard = ({ data, index }) => (
-  <Card>
-    <h1 className={styles[colors[index % 4]]}>{data.name}</h1>
-    <p>{data.email}</p>
-    <p>{data.phone}</p>
-  </Card>
-);
-
+const LoadingCard = ({ data, index }) => {
+  const color = colors[index % colors.length];
+  return (
+    <Card>
+      <h2 className={styles[`name_${color}`]}>{data.name}</h2>
+      <p>
+        <FaEnvelope className={styles[`icon_${color}`]} /> {data.email}
+      </p>
+      <p>
+        <FaPhone className={styles[`icon_${color}`]} /> {data.phone}
+      </p>
+    </Card>
+  );
+};
 export default LoadingCard;
